99d0195e13cb43f32e760f80844f680c94889dcc
okubicek <kubicek.ondrej@gmail.com>
2019-07-07 21:29:47 +0200

    flatsize parsing fix

--- FlatOffersTracker/Parsing/Collectors/SRealityCollector.cs
+++ FlatOffersTracker/Parsing/Collectors/SRealityCollector.cs
@@ -56,7 +56,7 @@ namespace FlatOffersTracker.Parsing.Collectors
 					NumberOfRooms = query.NumberOfRooms,
 					Address = el.FindElement(By.CssSelector(".locality")).GetAttribute("innerHTML"),
 					Price = ExtractPrice(el.FindElement(By.CssSelector(".norm-price")).GetAttribute("innerHTML")),
-					FlatSize = ExtractFlatSize(elements[0].FindElement(By.CssSelector(".name")).GetAttribute("innerHTML"))
+					FlatSize = ExtractFlatSize(el.FindElement(By.CssSelector(".name")).GetAttribute("innerHTML"))
 				}).ToList();
 			}
 
@@ -99,13 +99,16 @@ namespace FlatOffersTracker.Parsing.Collectors
 
 		private int ExtractFlatSize(string name)
 		{
-			var parts = name.Split("&nbsp;");
-			if (parts.Count() != 2)
+			var parts = name.Split(new string[] { "&nbsp;", " " }, StringSplitOptions.RemoveEmptyEntries);
+
+			var numberOfparts = parts.Count();
+
+			if (parts.Count() < 2)
 			{
 				return 0;
 			}
 
-			if (int.TryParse(parts[1], out var parsed))
+			if (int.TryParse(parts[numberOfparts - 2], out var parsed))
 			{
 				return parsed;
 			}
