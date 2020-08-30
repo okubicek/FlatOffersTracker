d8b9375cb978f7589fcdc18028cc47b25c2f76d9
okubicek <kubicek.ondrej@gmail.com>
2020-08-30 21:04:55 +0200

    Adding console app for unscheduled adhoc runs of advertisement polling.

--- FlatOffersTracker.DependencyInjection/Repository/WindsorContainerExtension.cs
+++ FlatOffersTracker.DependencyInjection/Repository/WindsorContainerExtension.cs
@@ -10,15 +10,30 @@ namespace FlatOffersTracker.DependencyInjection.Repository
 	{
 		public static void AddScopedDdContext(this WindsorContainer container, string connectionString)
 		{
-			Func<FlatOffersDbContext> create = () => 
-				new FlatOffersDbContext(new DbContextOptionsBuilder<FlatOffersDbContext>()
-				.UseSqlServer(connectionString, ob => ob.MigrationsAssembly(typeof(FlatOffersDbContext).Assembly.FullName))
-				.Options);
+			Func<FlatOffersDbContext> create = GetDbContextFactoryMethod(connectionString);
 
 			container.Register(Component
 				.For<FlatOffersDbContext>()
 				.UsingFactoryMethod(x => create())
 				.LifestyleScoped());
+		}
+
+		public static void AddTransientDdContext(this WindsorContainer container, string connectionString)
+		{
+			Func<FlatOffersDbContext> create = GetDbContextFactoryMethod(connectionString);
+
+			container.Register(Component
+				.For<FlatOffersDbContext>()
+				.UsingFactoryMethod(x => create())
+				.LifestyleTransient());
+		}
+
+		private static Func<FlatOffersDbContext> GetDbContextFactoryMethod(string connectionString)
+		{
+			return () =>
+				new FlatOffersDbContext(new DbContextOptionsBuilder<FlatOffersDbContext>()
+				.UseSqlServer(connectionString, ob => ob.MigrationsAssembly(typeof(FlatOffersDbContext).Assembly.FullName))
+				.Options);
 		}
 	}
 }
---
+++ FlatOffersTrackerConsoleApp/Program.cs
@@ -0,0 +1,60 @@
+﻿using Castle.Windsor;
+using Common.Cqrs;
+using FlatOffersTracker.DependencyInjection.Domain;
+using FlatOffersTracker.DependencyInjection.Repository;
+using Microsoft.Extensions.Configuration;
+using System;
+using Castle.Facilities.Logging;
+using Castle.Services.Logging.SerilogIntegration;
+using Serilog;
+using Serilog.Events;
+using Castle.MicroKernel.Registration;
+
+namespace FlatOffersTrackerConsoleApp
+{
+	public class Program
+	{
+		static void Main(string[] args)
+		{
+			ConfigureLogger();
+			var container = RegisterDependencies();
+
+			var trackerRunner = container.Resolve<ICommand>();			
+
+			trackerRunner.Execute();
+			Console.WriteLine("Execution has finished. Pleare press any key:");
+			Console.Read();
+		}
+
+		private static WindsorContainer RegisterDependencies()
+		{
+			var container = new WindsorContainer();			
+			container.Install(
+				new FlatOffersTrackerInstaller(),
+				new EFCoreRepositoryInstaller()
+			);
+			
+			//container.AddFacility<LoggingFacility>(f => f.LogUsing<SerilogFactory>());
+			IConfiguration Configuration = new ConfigurationBuilder()
+				.AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+				.Build();
+
+			container.Register(Component.For<ILogger>().Instance(Log.Logger));
+
+			var connectionString = Configuration.GetValue<string>("ConnectionStrings:DefaultConnection");
+			container.AddTransientDdContext(connectionString);
+
+			return container;
+		}
+
+		private static void ConfigureLogger()
+		{
+			Log.Logger = new LoggerConfiguration()
+				.MinimumLevel.Information()
+				.MinimumLevel.Override("Microsoft", LogEventLevel.Warning)
+				.Enrich.FromLogContext()
+				.WriteTo.RollingFile("Logs\\Log{Date}.txt", outputTemplate: "[{Timestamp:yyyy-MM-dd HH:mm:ss.fff zzz}] [{Level:u3}] {Message:lj}{NewLine}{Exception}")
+				.CreateLogger();
+		}
+	}
+}
